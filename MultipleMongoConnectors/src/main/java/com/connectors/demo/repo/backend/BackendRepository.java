64551d399cc866f7e66c1e39afa8b152ed065501
Walid7627 <wkhali95360@gmail.com>
2021-08-24 12:05:17 +0200

    Delete BackendRepository.java

--- MultipleMongoConnectors/src/main/java/com/connectors/demo/repo/backend/BackendRepository.java
+++
@@ -1,6 +0,0 @@
-package com.connectors.demo.repo.backend;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface BackendRepository extends ReactiveCrudRepository<Backend, String> {
-}
