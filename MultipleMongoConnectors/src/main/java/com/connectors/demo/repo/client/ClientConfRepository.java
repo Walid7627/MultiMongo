0cd847f872b679f5574973598e4c9750ec9e127e
Walid7627 <wkhali95360@gmail.com>
2021-08-24 12:06:54 +0200

    Delete MultipleMongoConnectors directory

--- MultipleMongoConnectors/src/main/java/com/connectors/demo/repo/client/ClientConfRepository.java
+++
@@ -1,8 +0,0 @@
-package com.connectors.demo.repo.client;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface ClientConfRepository extends ReactiveMongoRepository<ClientConf, String> {
-    Mono<ClientConf> findById(String id);
-}
